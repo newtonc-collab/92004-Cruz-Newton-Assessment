329028f2f6c22c6645b33636e268ba4719719afb
newtonc-collab <newtonc@stu.otc.school.nz>
2026-05-01 09:59:17 +1200

    Version 5 Added questions and score rating to finalize the code

--- My assessment code.py
+++ My assessment code.py
@@ -1,6 +1,6 @@
 #This program is a quiz to guess the first as well as sir names of new zealand actors who have had a role inside marvel movies
 #Variable Section (4 variables)
-questionsandanswersdict = {"Who played Odin in Thor Ragnarok?": "Sam Neill", "Who plays Korg in Avengers end game?": "Taika Waititi", "Who played Skurge in Thor Ragnarok?": "Karl-Hein"}
+questionsandanswersdict = {"Who played Odin in Thor Ragnarok?": "Sam Neill", "Who plays Korg in Avengers end game?": "Taika Waititi", "Who played Skurge in Thor Ragnarok?": "Karl Urban", "Who played Fire fist in Deadpool 2?": "Julian Dennison", "Who played Topaz in Thor ragnarok?": "Rachel House", "Who played Ashley Kafka in Spiderman 2?": "Marton Csokas", "Who played Rouge in X-Men: Days of Future Past?": "Anna Paquin", "And finally, who played ghost in Ant-Man and the Wasp? (hint: last name is 2 words)": "Hannah John Kamen"}
 questioniterator = iter(questionsandanswersdict.keys())
 answeriterator = iter(questionsandanswersdict.values())
 #Converting dict into something that I can iterate through with next function (3-5)
@@ -25,4 +25,15 @@ while True:
     else:
        print(f"That answer is wrong, the answer is {rightanswer}")
     #Checks if their answer is right by checking if the input is the same as the answer. If answer is right it will add score but if not it will print out the right answer. (22-26)
-print(score)
+if score < 2:
+   print(f"You only got a score of {score}, thats means you're an actual normal person and not a marvel geek.")
+elif score < 4:
+   print(f"You got a score of {score}, thats pretty impressive not gonna lie. But you can do better.")
+elif score < 6:
+   print(f"You got a score of {score}, that must mean you really know alot about marvel. Impressive indeed.")
+elif score < 8:
+   print(f"You got a score of {score}, this score is really impressive if you can actually remember some of those obscure ones.")
+elif score < 10:
+   print(f"You got a score of {score}, either you got this score because some of your answers were ai sources from reddit or you're just a geek.")
+elif score == 10:
+   print(f"You got a score of {score}, congrats on cheating to get here but if you didn't then you're just marvels biggest geek.")
