2db0c8d533e7838db8b1c6e4c07509953df0233d
newtonc-collab <newtonc@stu.otc.school.nz>
2026-05-04 13:16:26 +1200

    Version 6: Gave some more comments and also realized 2 major mistakes that were entirely my bad and i should of known aka: Remove {} from a print so you don't get {''}, as well as .capitalize should of been .title()

--- My assessment code.py
+++ My assessment code.py
@@ -1,15 +1,15 @@
 #This program is a quiz to guess the first as well as sir names of new zealand actors who have had a role inside marvel movies
 #Variable Section (4 variables)
-questionsandanswersdict = {"Who played Odin in Thor Ragnarok?": "Sam Neill", "Who plays Korg in Avengers end game?": "Taika Waititi", "Who played Skurge in Thor Ragnarok?": "Karl Urban", "Who played Fire fist in Deadpool 2?": "Julian Dennison", "Who played Topaz in Thor ragnarok?": "Rachel House", "Who played Ashley Kafka in Spiderman 2?": "Marton Csokas", "Who played Rouge in X-Men: Days of Future Past?": "Anna Paquin", "And finally, who played ghost in Ant-Man and the Wasp? (hint: last name is 2 words)": "Hannah John Kamen"}
+#Converting dict into something that I can iterate through with next function (5-7)
+#To keep score of how many answers are right (8)
+questionsandanswersdict= {"Who played Odin in Thor Ragnarok?": "Sam Neill", "Who plays Korg in Avengers end game?": "Taika Waititi", "Who played Skurge in Thor Ragnarok?": "Karl Urban", "Who played Fire fist in Deadpool 2?": "Julian Dennison", "Who played Topaz in Thor ragnarok?": "Rachel House", "Who played Ashley Kafka in Spiderman 2?": "Marton Csokas", "Who played Rouge in X-Men: Days of Future Past?": "Anna Paquin", "And finally, who played ghost in Ant-Man and the Wasp? (hint: last name is 2 words)": "Hannah John Kamen"}
 questioniterator = iter(questionsandanswersdict.keys())
 answeriterator = iter(questionsandanswersdict.values())
-#Converting dict into something that I can iterate through with next function (3-5)
 score = 0
-#To keep score of how many answers are right (7)
-#Quiz loop e
+#Quiz loop (10-27)
 while True:
     try:
-      print({next(questioniterator.strip("[]'"))})
+       print(next(questioniterator))
     except StopIteration:
       break
     #This will print out the question for people to see and will stop the quiz loop if there is nothing left to iterate it (11-14)
@@ -19,12 +19,14 @@ while True:
          break
       print("Please do not enter blank space, special characters, or numbers.")
     #Will ask for user to type an answer and if it contains nothing, special characters, or numbers it will ask again. (16-20)
-    rightanswer = next(answeriterator.strip("[]'"))
-    if userinput.capitalize == rightanswer:
+    rightanswer = next(answeriterator)
+    if userinput.title() == rightanswer:
        score = score + 1
     else:
        print(f"That answer is wrong, the answer is {rightanswer}")
     #Checks if their answer is right by checking if the input is the same as the answer. If answer is right it will add score but if not it will print out the right answer. (22-26)
+#End of quiz loop
+#Score ratings
 if score < 2:
    print(f"You only got a score of {score}, thats means you're an actual normal person and not a marvel geek.")
 elif score < 4:
